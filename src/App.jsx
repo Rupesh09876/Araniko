3b64526b9831a953d29b4157a23d792821d5e89c
Rupesh09876 <rupeshkatuwal53@gmail.com>
2026-08-18 15:34:25 +0545

    adding the path

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TopBar from "./components/layout/TopBar";
 import Navbar from "./components/layout/Navbar";
 import MobileBar from "./components/layout/MobileBar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
+import AllDoctors from "./pages/AllDoctors";
+import DoctorDetails from "./pages/DoctorDetails";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <TopBar />
       <Navbar />
       <main>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors" element={<AllDoctors />} />
+          <Route path="/doctors/:id" element={<DoctorDetails />} />
+        </Routes>
       </main>
       <Footer />
       <MobileBar />
-    </>
+    </BrowserRouter>
   );
 }
